6db5c844be766a2a9b69d9c0b74246d19fd238af
si3nloong <sianloong90@gmail.com>
2021-10-11 16:10:53 +0800

    fix: foreign key constraint

--- examples/transaction.go
+++ examples/transaction.go
@@ -196,6 +196,9 @@ func TransactionExamples(ctx context.Context, t *testing.T, db *sqlike.Database)
 		require.NoError(t, err)
 	}
 
+	err = db.Table("UserAddress").DropIfExists(ctx)
+	require.NoError(t, err)
+
 	table := db.Table("user")
 	err = table.DropIfExists(ctx)
 	require.NoError(t, err)
